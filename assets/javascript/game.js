4e4bea5e19ba18c2f5ddbcf06690b04af1d562f7
Ben Florentino <florentinob92@gmail.com>
2019-11-09 13:28:27 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,49 @@
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+            
 var wins = 0;
 var losses = 0;
-var guesses = 0;
-var guessesLeft = 9;
-var guessesMade = [];
-var letterToGuess = null;
+var left = 9;
+var guesses = 9;
+var guessesSoFar = [];
+var computerGuess;
 
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var newLetter = function() {
+   computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+};
+var soFar = function() {
+   document.getElementById("guesses").innerHTML = "Guesses So Far: " + guessesSoFar.join(",");
+};
+var guessesLeft = function() {
+   document.getElementById("left").innerHTML = "Guesses Left: " + left;
+};
+var newGame = function() {
+   left = 9;
+   newLetter();
+   guessesLeft();
+   guessesSoFar = [];
+}
+document.onkeyup = function(event) {
+   var userGuess = event.key;
+   console.log(userGuess);
+   if (userGuess == guessesSoFar) {
+       
+   } else if (userGuess == computerChoices) {
 
+    
+   };
+   left--;
+   guessesSoFar.push(userGuess);
+   soFar();
+   guessesLeft();
+   if (left > 0) {
+       if (userGuess == computerGuess) {
+           wins++;
+           document.getElementById("wins").innerHTML = "Wins:" + wins;
+           newGame();
+       }
+   } else if (left == 0) {
+       losses++;
+       document.getElementById("losses").innerHTML = "Losses:" + losses;
+       newGame();
+   } 
+};
